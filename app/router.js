0f0203950eba31264065ed72eabb6cd68367fa12
Miguel Madero <mmadero@practicefusion.com>
2014-11-02 00:45:01 -0700

    [WIP]

--- app/router.js
+++ app/router.js
@@ -6,6 +6,8 @@ var Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.resource('orders', function () {
+  });
   this.resource('products', function () {
     this.route('all', {path: '/all'});
     this.route('list', {path: '/:listId'});
